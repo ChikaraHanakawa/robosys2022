e1b5849f5e4289080a121428dba42716718a2d00
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2022-11-04 01:39:42 +0900

    upload yamada.bash

---
+++ test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Chikara Hanakawa
+# SPDX-License-Identifier: BSD-3-Clause
+　
+ng () {
+      echo NG at Line $1
+      res=1
+}
+res=0
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+　
+[ "$res" = 0 ] && echo OK
+exit $res
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
