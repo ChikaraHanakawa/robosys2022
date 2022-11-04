26bf000aad1826d1c2fe0900f599bac7f4b5b303
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2022-11-04 23:53:53 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -1,18 +1,23 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Chikara Hanakawa
 # SPDX-License-Identifier: BSD-3-Clause
-　
+
 ng () {
       echo NG at Line $1
       res=1
 }
+
 res=0
+
 ### I/O TEST ###
-out=$(seq 5 | ./plus)
+out=$(seq 5 | ./plus_stdin)
 [ "${out}" = 14 ] || ng ${LINENO}
-　
+
 [ "$res" = 0 ] && echo OK
 exit $res
+
+out=$(seq 5 | ./plus_stdin)
+[ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
