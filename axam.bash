8743a77237e31e7c43f9e569390abc3e64f375c1
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2022-11-27 12:21:26 +0900

    add matrix

---
+++ axam.bash
@@ -0,0 +1,3 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Chikara Hanakawa
+# SPDX-License-Identifier: BSD-3-Clause
---
+++ matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cv2
+import sys
+import numpy as np
+
+Keanu = np.array([0,1,2, [4,5,6]])
+Reeves = np.array([7,8,9, [10,11,12]])
+
+KeanuReeves1 = Keanu + Reeves
+
+print(KeanuReeves1)
